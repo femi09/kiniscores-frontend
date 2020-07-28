5e35209fb20ff9f0996eb7f0cb285bbccd9a61c7
Femi Omotayo <omotayooluwafemi66@gmail.com>
2020-07-28 20:10:32 +0100

    minor-bug-fixes

--- src/components/navItems.jsx
+++ src/components/navItems.jsx
@@ -6,7 +6,7 @@ const NavItems = () => {
     <div className="bg-gray-200 p-4 flex-row items-center">
       <ul className="flex center justify-center align-center">
         <li className="px-8 text-blue-800 font-semibold cursor-pointer hover:text-gray-500">
-          <Link to="/matches">Matches</Link>
+          <Link to="/">Matches</Link>
         </li>
         <li className="px-8 text-blue-800 font-semibold cursor-pointer hover:text-gray-500">
         <Link to="/standings">Standings</Link>
