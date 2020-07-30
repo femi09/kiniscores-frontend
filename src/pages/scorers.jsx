7d6f7bc0cadc4f198ee1cf02283e844ee341e987
Femi Omotayo <omotayooluwafemi66@gmail.com>
2020-07-30 09:00:21 +0100

    matches-group-by-matchday

--- src/pages/scorers.jsx
+++ src/pages/scorers.jsx
@@ -1,29 +1,33 @@
 import React, { useState, useEffect } from "react";
-import {footballApi} from '../config.json'
+import { footballApi } from "../config.json";
 import femi from "../services/httpService";
 import ScorersTable from "../components/scorersTable";
 
-const authToken =  process.env.REACT_APP_KINISCORES_API_KEY
+const authToken = process.env.REACT_APP_KINISCORES_API_KEY;
 
 const Scorers = () => {
   const [scorers, setScorers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const getScorers = async () => {
-      const {
-        data,
-      } = await femi.get(
-        `${footballApi}/scorers?limit=20`,
-        { headers: { "X-Auth-Token": authToken
-       } }
-      );
+      const { data } = await femi.get(`${footballApi}/scorers?limit=20`, {
+        headers: { "X-Auth-Token": authToken },
+      });
       setScorers(data.scorers);
+      setIsLoading(false);
     };
     getScorers();
   }, []);
   return (
     <div>
-      <ScorersTable scorers={scorers} />
+      {isLoading ? (
+        <div className="font-bold p-8 bg-white text-center text-2xl text-blue-700">
+          <h1>Loading Scorers...</h1>
+        </div>
+      ) : (
+        <ScorersTable scorers={scorers} />
+      )}
     </div>
   );
 };
--- src/pages/standings.jsx
+++ src/pages/standings.jsx
@@ -1,28 +1,33 @@
 import React, { useState, useEffect } from "react";
 import femi from "../services/httpService";
-import {footballApi} from '../config.json'
+import { footballApi } from "../config.json";
 import StandingTable from "../components/Standingtable";
 
-const authToken =  process.env.REACT_APP_KINISCORES_API_KEY
+const authToken = process.env.REACT_APP_KINISCORES_API_KEY;
 const Standings = () => {
   const [tables, setTables] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const getTable = async () => {
-      const {
-        data,
-      } = await femi.get(
-        `${footballApi}/standings`,
-        { headers: { "X-Auth-Token": authToken} }
-      );
+      const { data } = await femi.get(`${footballApi}/standings`, {
+        headers: { "X-Auth-Token": authToken },
+      });
       setTables(data.standings[0].table);
+      setIsLoading(false);
     };
 
     getTable();
   }, []);
   return (
     <div>
-      <StandingTable tables={tables} />
+      {isLoading ? (
+        <div className="font-bold p-8 bg-white text-center text-2xl text-blue-700">
+          <h1>Loading Table...</h1>
+        </div>
+      ) : (
+        <StandingTable tables={tables} />
+      )}
     </div>
   );
 };
