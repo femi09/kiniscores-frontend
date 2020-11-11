c9a5f305e7b219edd24edf73a2344f239fcc6e02
Femi Omotayo <omotayooluwafemi66@gmail.com>
2020-11-11 06:31:50 +0100

    deugging

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import NavBar from "./components/NavBar/MainNavBar";
-// import Results from "./pages/Results";
-// import Standings from "./pages/Standings";
-// import News from "./pages/TransferNews";
-// import Scorers from "./pages/Scorers";
+import Results from "./pages/Results";
+import Standings from "./pages/Standings";
+import News from "./pages/TransferNews";
+import Scorers from "./pages/Scorers";
 // import Fixtures from "./pages/Fixtures";
 import Home from "./pages/Home";
-// import AdminPanel from "./pages/Admin";
-// import NextFixtures from "./pages/NextFixtures";
-// import NewsBody from "./pages/NewsDetails";
-// import MatchDetails from "./pages/MatchDetails";
+import AdminPanel from "./pages/Admin";
+import NextFixtures from "./pages/NextFixtures";
+import NewsBody from "./pages/NewsDetails";
+import MatchDetails from "./pages/MatchDetails";
 import "./App.css";
 
 function App() {
@@ -18,15 +18,15 @@ function App() {
     <div className="App">
       <NavBar />
       <Switch>
-        {/* <Route path="/fixture/:id" component={MatchDetails} />
+        <Route path="/fixture/:id" component={MatchDetails} />
         <Route path="/admin" component={AdminPanel} />
         <Route path="/fixtures/:fixtureStatus" component={NextFixtures} />
-        <Route path="/fixtures" component={Fixtures} />
+        {/* <Route path="/fixtures" component={Fixtures} /> */}
         <Route path="/scorers" component={Scorers} />
         <Route path="/news/:id" component={NewsBody} />
         <Route path="/news" component={News} />
         <Route path="/standings" component={Standings} />
-        <Route path="/results" component={Results} /> */}
+        <Route path="/results" component={Results} />
         <Route path="/" component={Home} />
       </Switch>
     </div>
--- src/pages/NewsDetails/index.jsx
+++ src/pages/NewsDetails/index.jsx
@@ -1,65 +1,65 @@
-// import React, { useState, useEffect } from "react";
-// import { useParams } from "react-router-dom";
-// import { getLatestNewsBody } from "../../services/latestServices";
-// import { formatFixturesDate } from "../../utils/formatTime";
-// import { kiniscoresApi } from "../../config.json";
-// import SkeletonNewsBody from "../../components/Skeletons/News/SkeletonNewsbody";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getLatestNewsBody } from "../../services/latestServices";
+import { formatFixturesDate } from "../../utils/formatTime";
+import { kiniscoresApi } from "../../config.json";
+import SkeletonNewsBody from "../../components/Skeletons/News/SkeletonNewsbody";
 
-// const NewsBody = () => {
-//   const [news, setNews] = useState("");
-//   const [loading, setLoading] = useState(true);
-//   const { id } = useParams();
+const NewsBody = () => {
+  const [news, setNews] = useState("");
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
 
-//   useEffect(() => {
-//     const getNewsBody = async () => {
-//       const { data: news } = await getLatestNewsBody(id);
-//       setNews(news);
-//       setLoading(false);
-//     };
-//     getNewsBody();
-//   }, [id]);
+  useEffect(() => {
+    const getNewsBody = async () => {
+      const { data: news } = await getLatestNewsBody(id);
+      setNews(news);
+      setLoading(false);
+    };
+    getNewsBody();
+  }, [id]);
 
-//   const imageUrl = `${kiniscoresApi}/news/newsImage/${news._id}`;
-//   return (
-//     <div className="px-4 mx-2 sm:mx-4 xl:w-2/3 bg-gray-100 text-center xl:mx-auto">
-//       {loading ? (
-//         <SkeletonNewsBody />
-//       ) : (
-//         <div className="bg-gray-100 py-6 my-4">
-//           <div className="mx-2 sm:w-3/4 mx-auto">
-//             <h1 className="text-xl sm:text-2xl text-left text-blue-900 font-bold">
-//               {news.subTitle}
-//             </h1>
-//             <div className=" text-gray-600 py-4 text-xs font-bold sm:text-sm sm:font-semibold flex items-center xl:w-1/2">
-//               <svg
-//                 className="h-5 w-5 text-gray-600"
-//                 fill="none"
-//                 viewBox="0 0 24 24"
-//                 stroke="currentColor"
-//               >
-//                 <path
-//                   strokeLinecap="round"
-//                   strokeLinejoin="round"
-//                   strokeWidth="2"
-//                   d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
-//                 />
-//               </svg>
-//               <p className="border-r border-gray-500 px-1 font-bold sm:font-semibold">
-//                 {formatFixturesDate(news.createdAt)}
-//               </p>
-//               <p className="px-1">{news.source}</p>
-//             </div>
-//           </div>
-//           <div className="flex flex-col sm:w-3/4 mx-auto">
-//             <img src={imageUrl} alt="" />
-//             <p className="py-6 text-gray-800 text-left leading-9">
-//               {news.body}
-//             </p>
-//           </div>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
+  const imageUrl = `${kiniscoresApi}/news/newsImage/${news._id}`;
+  return (
+    <div className="px-4 mx-2 sm:mx-4 xl:w-2/3 bg-gray-100 text-center xl:mx-auto">
+      {loading ? (
+        <SkeletonNewsBody />
+      ) : (
+        <div className="bg-gray-100 py-6 my-4">
+          <div className="mx-2 sm:w-3/4 mx-auto">
+            <h1 className="text-xl sm:text-2xl text-left text-blue-900 font-bold">
+              {news.subTitle}
+            </h1>
+            <div className=" text-gray-600 py-4 text-xs font-bold sm:text-sm sm:font-semibold flex items-center xl:w-1/2">
+              <svg
+                className="h-5 w-5 text-gray-600"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              <p className="border-r border-gray-500 px-1 font-bold sm:font-semibold">
+                {formatFixturesDate(news.createdAt)}
+              </p>
+              <p className="px-1">{news.source}</p>
+            </div>
+          </div>
+          <div className="flex flex-col sm:w-3/4 mx-auto">
+            <img src={imageUrl} alt="" />
+            <p className="py-6 text-gray-800 text-left leading-9">
+              {news.body}
+            </p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
-// export default NewsBody;
+export default NewsBody;
